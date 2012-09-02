3e1733500b8ff4e8298f384ec9baec0090a77aa5
Mario Freitas <imkira@gmail.com>
2012-09-03 02:33:38 +0900

    env

--- .zsh/before/env.zsh
+++ .zsh/before/env.zsh
@@ -1,5 +1,3 @@
-export PATH=/usr/local/bin:$PATH
-
 export LC_ALL=en_US.UTF-8
 
 export ARCHFLAGS="-arch x86_64"
--- .zsh/before/node.zsh
+++ .zsh/before/node.zsh
@@ -1,2 +1,3 @@
 export NODE_PATH=/usr/local/lib/node_modules
 export NODE_PATH=/usr/local/lib/jsctags/:$NODE_PATH
+export PATH=./node_modules/.bin:/usr/local/share/npm/bin:$PATH
