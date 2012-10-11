f309813095840e528c89104adfdbee9b64307f04
Mário Freitas <imkira@gmail.com>
2012-10-11 14:32:13 +0900

    auto sdk folder fetching and put it on path

--- .zsh/before/android.zsh
+++ .zsh/before/android.zsh
@@ -1,1 +1,3 @@
-export ANDROID_SDK_ROOT=/usr/local/Cellar/android-sdk/r20
+export ANDROID_SDK_ROOT=/usr/local/Cellar/android-sdk/$(ls -1 /usr/local/Cellar/android-sdk/ | sort -r | head -n 1)
+export ANDROID_HOME=$ANDROID_SDK_ROOT
+export PATH=$PATH:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools
