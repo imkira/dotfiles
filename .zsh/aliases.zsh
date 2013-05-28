dddfe33ec0da163f6c0a1161a169d0dd2eb6588f
Mário Freitas <imkira@gmail.com>
2013-05-28 15:11:04 +0900

    added: gclone alias

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,3 +1,4 @@
 alias svn=colorsvn
 alias svndiff='svn diff --diff-cmd colordiff'
 alias grma='git ls-files --deleted -z | xargs -0 git rm'
+alias gclone='for branch in `git branch -r | grep -v HEAD | grep -v master `; do git branch --track ${branch#remotes/origin/} $branch; done'
