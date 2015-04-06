fba5f224d9c6cbfec173abc5c45eb681b8713a8f
Mário Freitas <imkira@gmail.com>
2015-04-06 23:08:56 +0900

    modified: node -> nvm

--- .zsh/before/nvm.zsh
+++ .zsh/before/nvm.zsh
@@ -2,4 +2,5 @@ export NODE_PATH=/usr/local/lib/node_modules
 export NODE_PATH=/usr/local/lib/jsctags/:$NODE_PATH
 export PATH=./node_modules/.bin:/usr/local/share/npm/bin:$PATH
 
+export NVM_DIR=$HOME/.nvm
 [[ -s $(brew --prefix nvm)/nvm.sh ]] && source $(brew --prefix nvm)/nvm.sh
