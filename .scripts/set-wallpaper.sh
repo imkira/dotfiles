881cd7a825a6e072c350fdea54bb41d808556200
Mário Freitas <imkira@gmail.com>
2021-01-03 05:39:41 +0900

    modified: static wallpaper per screen

--- .scripts/set-wallpaper.sh
+++ .scripts/set-wallpaper.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
 
-RESOLUTION=$(xrandr --current | head -n 1 | sed 's/^.*,\s*current \([0-9]\+\)\s*x\s*\([0-9]\+\)\s*,.*$/\1x\2/')
-SRC_URL="https://source.unsplash.com/featured/${RESOLUTION}/?beach"
-DST_FILE=~/.wallpaper.jpg
-
-if test -f "$DST_FILE"; then
-  feh --bg-scale "$DST_FILE"
-fi
-
-curl -q -L "$SRC_URL" -o "$DST_FILE"
+exec feh --bg-scale ~/.wallpapers/display1 ~/.wallpapers/display2
